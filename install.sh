520b05848c89313c33a869a86b809a6a16ffc6d7
Simon Baumann <simon.baumann@hhi.fraunhofer.de>
2021-06-28 15:26:56 +0200

    small corrections

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Set help message
-help_msg="install dotfiles: -h for this help, -work for git work_user, -home for git private user";
+help_msg="install dotfiles: -h for help, --work for git work_user, --home for git private user";
 backup_msg="-----Copying existing dotfiles to ~/.backup_dotfiles"
 symlink_msg="-----Creating symlinks for dotfiles"
 
@@ -13,17 +13,19 @@ if  [[ $1 = "" ]]; then
 	echo $help_msg;
 elif [[ $1 = "-h" ]]; then
 	echo $help_msg;
-elif [[ $1 = "-work" ]]; then
+elif [[ $1 = "--work" ]]; then
 	echo $backup_msg
 	"$DOTFILES_DIR/backup.sh";
 	cat "$DOTFILES_DIR/files/git_user_work" "$DOTFILES_DIR/files/git_config_wo_user" > "$DOTFILES_DIR/files/.gitconfig";
 	echo $symlink_msg	
 	"$DOTFILES_DIR/create_links.sh";
-elif [[ $1 = "-home" ]]; then
+elif [[ $1 = "--home" ]]; then
 	echo $backup_msg
 	"$DOTFILES_DIR/backup.sh";
 	cat "$DOTFILES_DIR/files/git_user_home" "$DOTFILES_DIR/files/git_config_wo_user" > "$DOTFILES_DIR/files/.gitconfig";
 	echo $symlink_msg	
 	"$DOTFILES_DIR/create_links.sh";
+else
+        echo $help_msg;
 fi
 
